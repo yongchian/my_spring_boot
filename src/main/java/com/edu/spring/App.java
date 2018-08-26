fc475213e6585239608629985c0fc13599befd51
chenyongqian <chenyongqian@sina.com>
2018-08-26 18:21:50 +0800

    lesson5

--- src/main/java/com/edu/spring/App.java
+++ src/main/java/com/edu/spring/App.java
@@ -1,17 +1,20 @@
 package com.edu.spring;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-/**
- * Hello world!
- *
- */
+@SpringBootApplication
 public class App {
+  
+  @Bean
+  public Runnable createRunnable() {
+    return () -> {System.out.println("spring is running!!!");};
+  }
+  
   public static void main(String[] args) {
-    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.edu.spring");
-    //System.out.println(context.getBean("createUser"));
-    //System.out.println(context.getBean(User.class));
-    context.getBean("user", User.class).show();
-    context.close();
+    ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+    context.getBean(Runnable.class).run();
   }
 }
