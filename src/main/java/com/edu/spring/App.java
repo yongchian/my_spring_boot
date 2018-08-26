83b02fd31d360feb8747c427250790095dac8c99
chenyongqian <chenyongqian@sina.com>
2018-08-26 18:39:56 +0800

    666

--- src/main/java/com/edu/spring/App.java
+++ src/main/java/com/edu/spring/App.java
@@ -9,9 +9,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
   public static void main(String[] args) {
     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.edu.spring");
-    //System.out.println(context.getBean("createUser"));
-    //System.out.println(context.getBean(User.class));
-    context.getBean("user", User.class).show();
+    context.getEnvironment().getProperty("local.ip");
     context.close();
   }
 }
