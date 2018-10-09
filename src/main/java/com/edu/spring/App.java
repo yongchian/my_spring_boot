ca6a8efd9d88a9e1acf29edced7a5dafc7c69a01
chenyongqian <chenyongqian@sina.com>
2018-10-10 00:12:56 +0800

    test

--- src/main/java/com/edu/spring/App.java
+++ src/main/java/com/edu/spring/App.java
@@ -2,15 +2,11 @@ package com.edu.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
 public class App {
   public static void main(String[] args) {
-    SpringApplication app = new SpringApplication(App.class);
-    //app.addInitializers(new MyApplicationContextInitializer());
-    ConfigurableApplicationContext context = app.run(args);
-    context.close();
+    SpringApplication.run(App.class, args);
   }
 }
---
+++ src/main/java/com/edu/spring/controller/RoleController.java
@@ -0,0 +1,12 @@
+package com.edu.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RoleController {
+  @GetMapping("/role/show")
+  public String show() {
+    return "role show";
+  }
+}
