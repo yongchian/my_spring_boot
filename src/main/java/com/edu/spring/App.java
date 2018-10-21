d791ad6bb33817dfe58cb0d9a2c8c1ae1cb86d2a
chenyongqian <chenyongqian@sina.com>
2018-10-21 18:29:25 +0800

    test

--- src/main/java/com/edu/spring/App.java
+++ src/main/java/com/edu/spring/App.java
@@ -4,13 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.edu.spring.boot.redis.EnableRedis;
 
+import redis.clients.jedis.Jedis;
+
+@EnableRedis
 @SpringBootApplication
 public class App {
   public static void main(String[] args) {
-    SpringApplication app = new SpringApplication(App.class);
-    //app.addInitializers(new MyApplicationContextInitializer());
-    ConfigurableApplicationContext context = app.run(args);
+    ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+    Jedis jedis = context.getBean(Jedis.class);
+    jedis.set("id", "aaa");
+    System.out.println("value is " + jedis.get("id"));
     context.close();
   }
 }
